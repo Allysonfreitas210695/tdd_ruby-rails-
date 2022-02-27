ce60bf1ae966c18a724b2875ce033de2a7af2308
Allysonfreitas210695 <allysonfr83@gmail.com>
2022-02-27 19:39:44 -0300

    one-liner stntax

--- exemplo_rspec_tdd/spec/matchers/range/range_spec.rb
+++ exemplo_rspec_tdd/spec/matchers/range/range_spec.rb
@@ -4,4 +4,9 @@ describe (1..10), "range" do
     expect(subject).to cover(2,5)
     expect(subject).not_to cover(0,11)
   end
+
+  it {is_expected.to cover(2)}
+  it {is_expected.to cover(2,5)}
+  it {is_expected.not_to cover(0)}
+
 end
