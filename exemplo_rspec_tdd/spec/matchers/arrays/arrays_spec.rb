9512e2e80ca5c36bc9c5d39f7743f52598a1e5a5
Allysonfreitas210695 <allysonfr83@gmail.com>
2022-03-01 01:04:52 -0300

    negativando matcheres

--- exemplo_rspec_tdd/spec/matchers/arrays/arrays_spec.rb
+++ exemplo_rspec_tdd/spec/matchers/arrays/arrays_spec.rb
@@ -1,7 +1,10 @@
+RSpec::Matchers.define_negated_matcher :exclude, :include
+
 describe Array.new([1,2,3,4,5]) do
   it "#include" do
     expect(subject).to include(2)
     expect(subject).to include(2,1)
+    expect(subject).to exclude(6)
   end
 
   it "#match_array" do
--- exemplo_rspec_tdd/spec/matchers/output/output_spec.rb
+++ exemplo_rspec_tdd/spec/matchers/output/output_spec.rb
@@ -1,4 +1,5 @@
 describe "Matcher output" do
   it { expect{puts "jackson"}.to output.to_stdout}
   it { expect{print "jackson"}.to output(/jackson/).to_stdout}
+  it { expect{print "jackson"}.to output("jackson").to_stdout}
 end
